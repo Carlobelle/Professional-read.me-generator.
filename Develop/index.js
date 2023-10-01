509bd8b69f9bd4ae55f6a9da14f8d8c1265f88a0
Carlovsky Bellefleur <carlovskybellefleur@Carlovskys-MacBook-Pro.local>
2023-09-30 22:58:54 -0300

    descriptions in package json

---
+++ Develop/index.js
@@ -0,0 +1,21 @@
+// TODO: Include packages needed for this application
+// title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: "input",
+        message:"what is your tittle?",
+        name:"tittle"
+},{
+    type: "input",
+    message:"what is your description?",
+    name:"description"
+},];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
